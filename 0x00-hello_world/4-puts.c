ccddff55eea342e0f20081d1834315746973c914
Nadia hamdy <hamdyn428@gmail.com>
2023-03-16 14:51:33 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -entry poit
- * return:always 0 (success)
+ * main - main block
+ * return:0
  */
 int main(void)
-  {  
-puts ("with proper grammar, but the outcome is a piece of art,\n");
+{  
+  puts("with proper grammar, but the outcome is a piece of art,\n");
   return (0);	
-  }
+}
