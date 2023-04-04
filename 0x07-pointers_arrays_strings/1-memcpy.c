2ea010476375b2f7366b4cf2db1e3a9ab01f6de5
Nadia hamdy <hamdyn428@gmail.com>
2023-04-03 17:03:38 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+*_memcpy - The _memcpy() function that copies memory area
+*@dest:area where bytes are copied to
+*@src:area where bytes are copied from
+*@n: function copies
+*Return: returns a pointer to @dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i < n)
+{
+src[i] = dest[i];
+i++
+}
+return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,11 +2,6 @@
 #define MAIN_H
 #include <stddef.h>
 #include <stdio.h>
-/**
-*void prototypes(void)
-*int prototypes(int)
-*char prototypes(char)
-*/
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
