16351a00321a40c55946106f1ad14ff504717fa4
Nadia hamdy <hamdyn428@gmail.com>
2023-04-05 11:07:18 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,13 @@
 *@s:character to be printed
 *Return:returns void
 */
-
 void _puts_recursion(char *s)
-if (*s == '\0')
-_putchar('\n');
-else
 {
-putchar(s[0]);
-_puts_recursion(s + 1);
-}
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
