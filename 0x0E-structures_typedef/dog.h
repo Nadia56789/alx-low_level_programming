18c6a533b9687f78dd5ac7e41c6214951c7482ad
Nadia hamdy <hamdyn428@gmail.com>
2023-04-17 09:30:34 -0700

    my commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+
+sruct dog
+{
+	char *name;
+	char *owner;
+	int age;
+};
