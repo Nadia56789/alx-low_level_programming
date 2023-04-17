2beccbde52bb509a15dcbb3bb521c72ca0242d84
Nadia hamdy <hamdyn428@gmail.com>
2023-04-17 10:05:15 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
 *struct dog - a new type of data
 *representing a dog
@@ -17,5 +16,13 @@ struct dog
 	char *owner;
 	float age;
 };
+
+/**
+*my_dog - Typedef for struct dog
+*/
+typedef struct dog my_dog;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
