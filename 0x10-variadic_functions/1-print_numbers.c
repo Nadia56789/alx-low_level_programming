5abcb2a6159464eab15c25930d17cd9a1733c13c
Nadia hamdy <hamdyn428@gmail.com>
2023-04-20 16:08:27 -0700

    1-print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,17 +12,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
 
-	int i, sum = 0;
+	unsigned int i, sum = 0;
 
 	if (n == 0)
-		return (0)
+		return (0);
 			va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		sum = sum + va_arg(list, int)
+		sum = sum + va_arg(list, int);
 	}
 
-	va_end(list)
+	va_end(list);
 		return (sum);
 
 
