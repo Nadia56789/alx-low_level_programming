fac23281deaf434066f517f84b979abb9e4e18dd
Nadia hamdy <hamdyn428@gmail.com>
2023-04-19 18:20:24 -0700

    my commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+*array_iterator - executes a function on each element of an array
+*@array: the array
+*@size: size of array
+*@action:  executes a function given as a parameter on each element of an array
+*
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	if (array || action)
+
+		for (i = 0; i < size; i++)
+		action(array[i]);
+}
