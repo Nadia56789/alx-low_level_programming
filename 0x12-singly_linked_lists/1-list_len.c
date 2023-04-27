8a807e5d355496e7393b944cfd1b86217f60f74d
Nadia hamdy <hamdyn428@gmail.com>
2023-04-27 15:24:44 -0700

    my commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - returns then number of elements in a list.
+ * @s: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *s)
+{
+	size_t cpt = 0;
+
+	while(s != NULL)
+
+	{
+		s = s->next;
+	}
+
+	cpt++;
+
+	return (cpt);
+}
