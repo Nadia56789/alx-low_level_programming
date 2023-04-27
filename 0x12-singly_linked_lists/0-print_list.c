3e23e8c289b61712d0ed5d253a71f5690669aec3
Nadia hamdy <hamdyn428@gmail.com>
2023-04-27 14:52:53 -0700

    0-printf_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,9 @@ size_t print_list(const list_t *h)
 
 size_t cpt = 0;
 
-while (h)
+while (h != NULL)
 {
-if (h->str)
+if (h->str != NULL)
 printf("[%d] %s\n", h->len, h->str);
 else
 printf("[0] (nil)\n");
