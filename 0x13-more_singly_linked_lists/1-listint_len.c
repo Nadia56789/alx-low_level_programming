5ed45f97dc8c8b7f4e2ce8310fa3e14c23855bf9
Nadia hamdy <hamdyn428@gmail.com>
2023-05-01 05:11:53 -0700

    my commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * listint_t - returns then number of elements in a list.
- * @s: singly linked list.
+ * listint_len - returns then number of elements in a list.
+ * @h: singly linked list.
  * Return: number of elements in the list.
  */
 
@@ -14,7 +14,7 @@ size_t listint_len(const listint_t *h)
 	while (h != NULL)
 
 	{
-		
+
 		h = h->next;
 		cpt++;
 	}
