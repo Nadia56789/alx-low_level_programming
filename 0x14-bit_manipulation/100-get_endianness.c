d888beaa8a985eeb993b9e3146a757d6a9f4311f
Nadia hamdy <hamdyn428@gmail.com>
2023-05-04 18:46:54 -0700

    my commit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
  /**
- * get_endianness - checks the endianness
- * Return: 0 if big endian, 1 if little endian
+ *get_endianness - checks the endianness
+ *Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
