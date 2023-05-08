b0f936d757ceee618c0c8ef0e5a8cce3772be91c
Nadia hamdy <hamdyn428@gmail.com>
2023-05-08 15:21:29 -0700

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,26 +9,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int m;
+	int fd;
 	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
-	m = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (m == -1)
+	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
-	nrd = read(m, buf, letters);
+	nrd = read(fd, buf, letters);
 	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	close(m);
+	close(fd);
 
 	free(buf);
 
